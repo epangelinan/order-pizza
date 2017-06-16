d49f29a99b9ca7e56c2cf82835a66887e56403e2
Esvethlynna Pangelinan <esveth@aol.com>
2017-06-16 15:41:05 -0700

    put result in a column next to checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //Business Logic
-var toppings = [];
 var size = "";
-var toppingSelected;
-var finalCost = 0;
+var toppings = [];
+var toppingSelected = "";
 var cost = 0;
+var finalCost = 0;
 
 //A pizza object constructor with properties for toppings and size.
 function Pizza(toppings, size) {
@@ -14,39 +14,35 @@ function Pizza(toppings, size) {
 //A prototype method for the cost of a pizza depending on the selections chosen.
 Pizza.prototype.cost = function () {
   if (this.size === "Small") {
-    cost = (this.toppings.length + 10) * 1.1;
+    cost = (this.toppings.length + 10) * 1.101;
     return cost;
   } else if (this.size === "Medium") {
-    cost = (this.toppings.length + 12) * 1.1;
+    cost = (this.toppings.length + 12) * 1.101;
     return cost;
   } else if (this.size === "Large") {
-    cost = (this.toppings.length + 14) * 1.1;
+    cost = (this.toppings.length + 14) * 1.101;
     return cost;
   } else if (this.size === "X-Large") {
-    cost = (this.toppings.length + 16) * 1.1;
+    cost = (this.toppings.length + 16) * 1.101;
     return cost;
   }
 }
-
-
 
 //UI Logic
 $(document).ready(function() {
   //Allow the user to choose toppings and size for the pizza they'd like to order.
   $("form").submit(function(event) {
     event.preventDefault();
-    //alert("submit successfull!");
     size = $("#size").val();
-    alert(size);
+
     $("input:checkbox[name=topping]:checked").each(function() {
       toppingSelected = $(this).val();
       toppings.push(toppingSelected);
     });
-    alert(toppings);
+
     var newPizza = new Pizza(toppings, size);
     finalCost = newPizza.cost().toFixed(2);
     $("#result").show();
     $("#total-cost").text(finalCost);
   });
-
 });
