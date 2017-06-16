4deff25c0f742fb698b07452df052638fac35d9a
Esvethlynna Pangelinan <esveth@aol.com>
2017-06-16 09:53:02 -0700

    add code for constructor and necessary variables

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+//Business Logic
+
+//Create a pizza object constructor with properties for toppings and size.
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+//Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+Pizza.prototype.cost = function () {
+  if (this.size === "small") {
+    alert(10 * 1.1);
+    return 10 * 1.1;
+  } else if (this.size === "medium") {
+    alert(12 * 1.1);
+    return(12 * 1.1);
+  } else if (this.size === "large") {
+    alert(14 * 1.1);
+    return(14 * 1.1);
+  }
+}
+
+var toppings = [];
+var size = "";
+
+var newPizza = new Pizza("cheese", "small");
+newPizza.cost();
+
+
+
+//UI Logic
+$(document).ready(function() {
+  //alert("document ready");
+  //Allow the user to choose toppings and size for the pizza they'd like to order.
+});
